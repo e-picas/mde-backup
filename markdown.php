1be163ed258d876262ad6c8c7f28ad4367405e05
Piero Wbmstr <me@e-piwi.fr>
2012-11-01 10:11:50 +0100

    OK, the full version of PHP Extended Markdown seems to be ready :)
    Cleaning all other versions, I just let the PHP Extended one, last goal
    of the project.

---
+++ markdown.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * PHP Extended Markdown
+ * Copyright (c) 2004-2012 Pierre Cassat
+ *
+ * original MultiMarkdown
+ * Copyright (c) 2005-2009 Fletcher T. Penney
+ * <http://fletcherpenney.net/>
+ *
+ * original PHP Markdown & Extra
+ * Copyright (c) 2004-2012 Michel Fortin  
+ * <http://michelf.com/projects/php-markdown/>
+ *
+ * Original Markdown
+ * Copyright (c) 2004-2006 John Gruber  
+ * <http://daringfireball.net/projects/markdown/>
+ */
+
+// -----------------------------------
+// LIBRARY
+// -----------------------------------
+
+require_once __DIR__."/PHP_Extended_Markdown.class.php";
+
+// -----------------------------------
+// STANDARD FUNCTION INTERFACE
+// -----------------------------------
+
+/**
+ * Initialize the parser and return the result of its transform method
+ */
+function Markdown($text) {
+	// Setup static parser variable
+	static $parser;
+	if (!isset($parser))
+	{
+		$parser = new PHP_Extended_Markdown_Parser;
+	}
+	// Transform text using parser
+	return $parser->transform($text);
+}
+
+/**
+ * Use the Markdown Extended command line interface
+ */
+function Markdown_CLI() {
+	// Setup static console variable
+	static $console;
+	if (!isset($console))
+	{
+		$console = new PHP_Extended_Markdown_Console;
+	}
+	// run the interface
+	return $console->run();
+}
+
+// -----------------------------------
+// COMMAND LINE INTERFACE
+// -----------------------------------
+
+if (php_sapi_name() == 'cli')
+{
+	require_once __DIR__."/PHP_Extended_Markdown_Console.class.php";
+	Markdown_CLI();
+}
+
+// Endfile
